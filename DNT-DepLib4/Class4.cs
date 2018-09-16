30e91e2d3f5e205a880dbe82415aafca45b82ce2
Daiyuu Nobori <daiyuu.git@softether.co.jp>
2018-09-16 12:47:47 +0900

    aaa

---
+++ DNT-DepLib4/Class4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib4
+{
+    public class LibClass4
+    {
+        public static void Hello()
+        {
+            Lib1.LibClass1.Hello();
+        }
+    }
+}
---
+++ DNT-DepLibTestApp4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DNT_DepLibTestApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lib4.LibClass4.Hello();
+        }
+    }
+}
